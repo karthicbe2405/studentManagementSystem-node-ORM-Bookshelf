e3072f2870021f8bb98ade35663290b0b24ebb78
karthicbe2405 <karthicbe2405@gmail.com>
2021-07-22 17:41:37 +0530

    Added Manual Transaction in addingPersonal Detail

--- app/services/student/studentService.js
+++ app/services/student/studentService.js
@@ -1,6 +1,9 @@
 let { json } = require('body-parser');
 let Student = require('../../models/student');
 let PersonalDetail = require('../../models/personaldetail');
+let bookshelf = require('../../utils/db');
+let Promise = require('bluebird');
+
 class student  {
       async fetchMarks(req,res){
         await Student.where({"registerNumber":req.query.registerNumber}).fetch(
@@ -44,19 +47,70 @@ class student  {
 
         await Student.where({"registerNumber":req.body.registerNumber}).fetch()
         .then( data => {
+
             personalDetail.student_id = data.id;
-            PersonalDetail.forge(personalDetail).save()
-            .then( data2 => {
-                return res.status(200).json(data2.toJSON());
+
+            bookshelf.transaction( function(transacting){
+                
+                PersonalDetail.forge(personalDetail).save(null , {transacting})
+                .then( data2 => {
+
+                    transacting.commit();
+
+                    return res.status(200).json(data2.toJSON());
+                })
+                .catch( err => {
+
+                    transacting.rollback();
+
+                    return res.status(400).json({"Message" : "Something Went Wrong"});
+                })
             })
-            .catch( err => {
-                console.log(err);
-                return res.status(400).json({"Message" : "Something Went Wrong"});
-            })
+            
         })
         .catch( err => {
-            console.log(err);
+
             return res.status(400).json({"Message" : "Something Went Wrong"});
+
+        })
+    }
+
+    async addPersonalDetailPromise(req,res){
+
+        let personalDetail = {
+            fathername : req.body.fathername,
+            mobilenumber : req.body.mobilenumber,
+            address : req.body.address,
+            student_id : null
+        }
+
+        await Student.where({"registerNumber":req.body.registerNumber}).fetch()
+        .then( data => {
+
+            personalDetail.student_id = data.id;
+
+            bookshelf.transaction( function(transacting){
+                
+                PersonalDetail.forge(personalDetail).save(null , {transacting})
+                .then( data2 => {
+
+                    transacting.commit();
+                    
+                    return res.status(200).json(data2.toJSON());
+                })
+                .catch( err => {
+
+                    transacting.rollback();
+
+                    return res.status(400).json({"Message" : "Something Went Wrong"});
+                })
+            })
+            
+        })
+        .catch( err => {
+
+            return res.status(400).json({"Message" : "Something Went Wrong"});
+
         })
     }
 }
